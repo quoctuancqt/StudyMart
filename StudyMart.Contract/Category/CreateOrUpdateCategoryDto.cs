4ae0256562cddfd270d04251155e3689d3d940cf
Tuan Cao <quoctuan.cqt@gmail.com>
2024-11-27 19:00:44 +0700

    - Add Contract project

---
+++ StudyMart.Contract/Category/CreateOrUpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyMart.Contract.Category;
+
+public class CreateOrUpdateCategoryDto(string name)
+{
+    [Required] 
+    public string Name { get; private set; } = name;
+}
