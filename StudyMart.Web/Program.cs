64030f721ef37b886eff66a3d9c5623a52929275
Tuan Cao <quoctuan.cqt@gmail.com>
2024-12-13 10:33:30 +0700

    Handle Auto refresh token when access token is expired.

--- StudyMart.Web/Program.cs
+++ StudyMart.Web/Program.cs
@@ -1,3 +1,5 @@
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -65,7 +67,58 @@ builder.Services.AddAuthentication(options =>
             options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             options.UsePkce = true;
         })
-    .AddCookie();
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.Events = new CookieAuthenticationEvents
+        {
+            // After the auth cookie has been validated, this event is called.
+            // In it we see if the access token is close to expiring.  If it is
+            // then we use the refresh token to get a new access token and save them.
+            // If the refresh token does not work for some reason then we redirect to 
+            // the login screen.
+            OnValidatePrincipal = async cookieCtx =>
+            {
+                var now = DateTimeOffset.UtcNow;
+                var expiresAt = cookieCtx.Properties.GetTokenValue("expires_at");
+                var accessTokenExpiration = DateTimeOffset.Parse(expiresAt!);
+                var timeRemaining = accessTokenExpiration.Subtract(now);
+                // TODO: Get this from configuration with a fall back value.
+                var refreshThresholdMinutes = 1440;
+                var refreshThreshold = TimeSpan.FromMinutes(refreshThresholdMinutes);
+
+                if (timeRemaining < refreshThreshold)
+                {
+                    var refreshToken = cookieCtx.Properties.GetTokenValue("refresh_token");
+                    // TODO: Get this HttpClient from a factory
+                    var httpClientFactory = cookieCtx.HttpContext.RequestServices.GetRequiredService<IHttpClientFactory>();
+                    var client = httpClientFactory.CreateClient();
+                    var response = await client.RequestRefreshTokenAsync(new RefreshTokenRequest
+                    {
+                        Address = "http://localhost:8080/realms/study-mart/protocol/openid-connect/token",
+                        ClientId = "web",
+                        RefreshToken = refreshToken!
+                    });
+
+                    if (!response.IsError)
+                    {
+                        var expiresInSeconds = response.ExpiresIn;
+                        var updatedExpiresAt = DateTimeOffset.UtcNow.AddSeconds(expiresInSeconds);
+                        cookieCtx.Properties.UpdateTokenValue("expires_at", updatedExpiresAt.ToString());
+                        cookieCtx.Properties.UpdateTokenValue("access_token", response.AccessToken!);
+                        cookieCtx.Properties.UpdateTokenValue("refresh_token", response.RefreshToken!);
+                        
+                        // Indicate to the cookie middleware that the cookie should be remade (since we have updated it)
+                        cookieCtx.ShouldRenew = true;
+                    }
+                    else
+                    {
+                        cookieCtx.RejectPrincipal();
+                        await cookieCtx.HttpContext.SignOutAsync();
+                    }
+                }
+            }
+        };
+    });
 
 builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
 builder.Services.AddCascadingAuthenticationState();
